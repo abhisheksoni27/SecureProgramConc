de56cccf58665a4847c287704d743ffe8ec940eb
yeziyqf <511198980@qq.com>
2018-03-06 04:41:26 -0500

    Completed User Feedback page.

--- src/components/pages/dashboard/Reports/Reports.jsx
+++ src/components/pages/dashboard/Reports/Reports.jsx
@@ -7,19 +7,40 @@ var Buttons = React.createClass({
     return (
 
       <div key="reports" className="reports-page">
-        <div className="ng-scope"> 
-          <Link to="/dashboard/overview" className="pull-right btn btn-primary btn-outline btn-rounded">Back to Overview</Link> 
-          <h2>Reports <small>Feedback Survey</small></h2> 
+        <div className="ng-scope">
+          <Link to="/dashboard/overview" className="pull-right btn btn-primary btn-outline btn-rounded">Back to Overview</Link>
+          <h2>Reports <small>Feedback Survey</small></h2>
 
-          <i className="glyphicon glyphicon-dashboard bg-fade"></i>
-          <Jumbotron> 
-            <h1>Add Comments here</h1> 
-            <p>You can give us your suggestions and feedback</p> 
-            <p> <a className="btn btn-primary btn-lg btn-outline btn-rounded">Submit</a> </p> 
-          </Jumbotron> 
+          {/* <i className="glyphicon glyphicon-dashboard bg-fade"></i> */}
+          <Jumbotron>
+            <h1>Add Comments here</h1>
+            <p>You can give us your suggestions and feedback</p>
+            <div class="container">
+              <form action="action_page.php">
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
+
+                  <label for="lname">Last Name</label>
+                  <input type="text" id="lname" name="lastname" placeholder="Your last name.."></input>
+
+                  <label for="country">Feedback Category</label>
+                  <select id="default" name="Feedback Category">
+                    <option value="Suggestions">Suggestions</option>
+                    <option value="Questions">Questions</option>
+                    <option value="Error Report">Error Report</option>
+                  </select>
+
+                  <label for="subject">Details</label>
+                  <textarea id="subject" name="subject" placeholder="Write something.." height="200"></textarea>
+
+                  <input type="submit" value="Submit"></input>
+              </form>
+            </div>
+            {/* <p> <a className="btn btn-primary btn-lg btn-outline btn-rounded">Submit</a> </p> */}
+          </Jumbotron>
         </div>
       </div>
-      
+
     );
   }
 
